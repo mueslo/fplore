d9df3780404961284409e169d9fd8c27e6d25abe
mueslo <johannesfalke@googlemail.com>
2019-02-12 15:54:12 +0100

    Bump version to 0.3.1a1

--- fplore/__init__.py
+++ fplore/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = "0.3.1a"
+__version__ = "0.3.1a1"
 __author__ = "Johannes Falke <johannesfalke@gmail.com>"
 __copyright__ = "Copyright (C) 2018 Johannes Falke"
 __license__ = "GNU General Public License v3"
