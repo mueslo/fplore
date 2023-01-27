6c54c54873bbd799db8680b2e2dfcc553d217149
Johannes Falke <johannesfalke@gmail.com>
2023-01-27 18:02:34 +0100

    parse DOS sections correctly

--- fplore/files/dos.py
+++ fplore/files/dos.py
@@ -10,26 +10,31 @@ from ..logging import log
 
 
 class DOS(FPLOFile):
-    __fplo_file__ = re.compile(r"\+dos\..+")
+    __fplo_file__ = re.compile(r"\+i?l?n?dos\..+")
 
     @loads('data', 'header')
     def load(self):
         dos_file = open(self.filepath, 'r')
 
-        header = next(dos_file)
-        # todo: parse header & filename
-        log.debug(header)
-        header = {label: int(val) if label != 'nl' else val
-                  for label, _, val in
-                  zip_longest(*[iter(header.split()[1:])] * 3)}
+        sections = dict()
 
-        data = []
         for line in dos_file:
-            ls = line.split()
-            if len(ls) == 2:
-                data.append(tuple(map(float, line.split())))
+            if line.startswith('#'):
+                header = line
+                log.debug(header)
+                header = {label: int(val) if label != 'nl' else val
+                          for label, _, val in
+                          zip_longest(*[iter(header.split()[1:])] * 3)}
+                log.debug(header)
+                header = tuple(header.items())
+                log.debug(header)
+                sections[header] = []
+            else:
+                ls = line.split()
+                if len(ls) == 2:
+                    sections[header].append(tuple(map(float, line.split())))
 
-        return np.array(data, dtype=[
-            ('e', 'f4'),
-            ('dos', 'f4'),
-        ]), header
+        headers = list(sections.keys())
+        data = [np.array(sections[hdr], dtype=[('e', 'f4'), ('dos', 'f4')]) for hdr in sections.keys()]
+
+        return data, headers
