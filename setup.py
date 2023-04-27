9e31c32f779074bc7d2cc88dbf114f9c0245abab
Johannes Falke <johannesfalke@gmail.com>
2023-04-27 21:26:30 +0200

    move to src layout

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy as np
 
 setup(
     ext_modules=[
-        Extension('fplore.fast_util', sources=['fplore/fast_util.pyx'],
+        Extension('fplore.fast_util', sources=['src/fplore/fast_util.pyx'],
                   include_dirs=[np.get_include()]),
     ],
 )
