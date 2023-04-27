252a52b08d9f8c1a309d5ae49fa9275cf5b2b82f
Johannes Falke <johannesfalke@gmail.com>
2023-04-27 19:43:06 +0200

    fix build

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, Extension
-
+import numpy as np
 
 setup(
     ext_modules=[
-        Extension('fplore.fast_util', sources=['fplore/fast_util.pyx']),
+        Extension('fplore.fast_util', sources=['fplore/fast_util.pyx'],
+                  include_dirs=[np.get_include()]),
     ],
 )
