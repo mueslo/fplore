d58a7cbff7dbe9c76c401217e4a7d9cbb04a190d
Johannes Falke <johannesfalke@gmail.com>
2023-05-03 20:21:52 +0200

    bump version

--- src/fplore/__init__.py
+++ src/fplore/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5b1"
+__version__ = "0.5"
 __author__ = "Johannes Falke <johannesfalke@gmail.com>"
 __copyright__ = "Copyright (C) 2018-2023 Johannes Falke"
 __license__ = "GNU General Public License v3"
