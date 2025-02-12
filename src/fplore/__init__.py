c13bbf51039308c6e677790245abac702c28deed
mueslo <johannesfalke@googlemail.com>
2025-02-12 10:46:05 +0100

    Bump version to 0.6.1

--- src/fplore/__init__.py
+++ src/fplore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.6"
+__version__ = "0.6.1"
 __author__ = "Johannes Falke <johannesfalke@gmail.com>"
-__copyright__ = "Copyright (C) 2018-2023 Johannes Falke"
+__copyright__ = "Copyright (C) 2018-2025 Johannes Falke"
 __license__ = "GNU General Public License v3"
 
 from .run import FPLORun  # noqa: F401
